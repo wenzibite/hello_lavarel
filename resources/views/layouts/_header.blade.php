d4dc77603ed474397a7acf954fb5f084bc5842a5
wenbing <361052550@qq.com>
2020-05-22 20:23:30 +0800

    切割头部和尾部子视图

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,9 @@
+<nav class="navbar navbar-inverse navbar-expand-lg navbar-static-top">
+    <div class="container">
+    <a class="navbar-brand" href="/">Weibo App</a>
+    <ul class="nav navbar-nav navbar-right">
+        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
+    </ul>
+    </div>
+</nav>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -9,7 +9,7 @@
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@3.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/app.css">
+    <link href="/css/app.css?id=1" rel="stylesheet">
 
     <!-- HTML5 shim 和 Respond.js 是为了让 IE8 支持 HTML5 元素和媒体查询（media queries）功能 -->
     <!-- 警告：通过 file:// 协议（就是直接将 html 页面拖拽到浏览器中）访问页面时 Respond.js 不起作用 -->
@@ -19,20 +19,13 @@
     <![endif]-->
   </head>
   <body>
-    <nav class="navbar navbar-inverse navbar-expand-lg navbar-static-top">
-      <div class="container">
-        <a class="navbar-brand" href="/">Weibo App</a>
-        <ul class="nav navbar-nav navbar-right">
-            <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
-          <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
-        </ul>
-      </div>
-    </nav>
+    @include('layouts._header')
 
     <div class="container">
       @yield('content')
+      @include('layouts._footer')
     </div>
-
+    
     <!-- jQuery (Bootstrap 的所有 JavaScript 插件都依赖 jQuery，所以必须放在前边) -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@1.12.4/dist/jquery.min.js"></script>
     <!-- 加载 Bootstrap 的所有 JavaScript 插件。你也可以根据需要只加载单个插件。 -->
