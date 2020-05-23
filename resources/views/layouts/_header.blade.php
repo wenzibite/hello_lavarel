3e712fd7a85ec143f246a8adad7fd6e486725c37
wenbing <361052550@qq.com>
2020-05-23 11:09:03 +0800

    命名路由

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -1,8 +1,8 @@
 <nav class="navbar navbar-inverse navbar-expand-lg navbar-static-top">
     <div class="container">
-    <a class="navbar-brand" href="/">Weibo App</a>
+    <a class="navbar-brand" href="{{ route('home') }}">Weibo App</a>
     <ul class="nav navbar-nav navbar-right">
-        <li class="nav-item"><a class="nav-link" href="/help">帮助</a></li>
+        <li class="nav-item"><a class="nav-link" href="{{ route('help') }}">帮助</a></li>
         <li class="nav-item" ><a class="nav-link" href="#">登录</a></li>
     </ul>
     </div>
