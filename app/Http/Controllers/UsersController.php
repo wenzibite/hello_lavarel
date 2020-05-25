0ba4cb751cf0c6a81e6c5cccb9869f01eec917da
wenbing <361052550@qq.com>
2020-05-25 21:30:18 +0800

    登录成功后导航逻辑

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use Auth;
 class UsersController extends Controller
 {
 
@@ -36,6 +37,9 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+
+        //自动登录
+        Auth::login($user);
 
         //注册成功后页面顶部提示信息
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
