3ef28ef408ea717c40fafa0847525b0d6187d02f
wenbing <361052550@qq.com>
2020-05-27 21:31:53 +0800

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,7 +34,8 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        return view('users.show', compact('user'));
+        $statuses = $user->statuses()->orderBy('created_at', 'desc')->paginate(10);
+        return view('users.show', compact('user','statuses'));
     }
 
     //
@@ -112,7 +113,7 @@ class UsersController extends Controller
         // });
         Mail::send($view, $data, function ($message) use ($to, $subject) {
             $message->to($to)->subject($subject);
-        })
+        });
     }
 
     //激活
