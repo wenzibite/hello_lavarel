769058b2f0359dbbd849212f8c906ab82c93570c
wenbing <361052550@qq.com>
2020-05-28 11:32:33 +0800

    首页微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -34,7 +34,7 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        $statuses = $user->statuses()->orderBy('created_at', 'desc')->paginate(10);
+        $statuses = $user->feed()->paginate(10);
         return view('users.show', compact('user','statuses'));
     }
 
