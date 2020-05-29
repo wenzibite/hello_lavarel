a77a75a3e752b0882cc43043677535e494c3879d
wenbing <361052550@qq.com>
2020-05-29 14:14:49 +0800

    关注和粉丝列表页面

--- routes/web.php
+++ routes/web.php
@@ -45,3 +45,7 @@ Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('passw
 
 //微博路由
 Route::resource('statuses', 'StatusesController', ['only'=>['store', 'destroy']]);
+//关注者列表
+Route::get('/users/{user}/followings', 'UsersController@followings')->name('users.followings');
+//粉丝列表
+Route::get('/users/{user}/followers', 'UsersController@followers')->name('users.followers');
